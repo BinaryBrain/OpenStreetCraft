2b17ee86688ef53283ac48e410728304502b0521
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-05-10 18:16:11 +0200

    OSM data extractor

---
+++ getOSMData/main.js
@@ -0,0 +1,55 @@
+// usage : node main.js 6.563243099999999 46.518709099999995
+var osmread = require('osm-read');
+
+if (process.argv.length < 4) {
+    console.log('failed');
+}
+
+var DEG_TO_METER = 111111;
+var ONE_METER = 0.000009;
+var SQUARE_SIZE = 500;
+
+var longitude = process.argv[2];
+var latitude = process.argv[3];
+
+var minLongitude = longitude - ONE_METER * SQUARE_SIZE;
+var maxLongitude = longitude + ONE_METER * SQUARE_SIZE;
+
+var minLatitude = latitude - ONE_METER * SQUARE_SIZE;
+var maxLatitude  = latitude + ONE_METER * SQUARE_SIZE;
+
+
+var OSM_DATA_BLOB = {
+    longitude : longitude,
+    latitude  :latitude,
+    minLatitude : minLatitude,
+    maxLatitude : maxLatitude,
+    minLongitude : minLongitude,
+    maxLongitude : maxLongitude,
+    way : [],
+    node : [],
+    bounds : []
+};
+
+osmread.parse({
+    url: 'http://www.overpass-api.de/api/xapi?way[bbox=' + minLongitude+ ',' + minLatitude + ',' + maxLongitude + ',' + maxLatitude + ']',
+    format: 'xml',
+    endDocument: function() {
+        console.log(JSON.stringify(OSM_DATA_BLOB));
+    },
+    bounds: function(bounds){
+        OSM_DATA_BLOB.bounds.push(bounds);
+    },
+    node: function(node){
+        node.lon = (node.lon - longitude) * 111111 | 0;
+        node.lat = (node.lat - latitude) * 111111 | 0;
+        OSM_DATA_BLOB.node.push(node);
+    },
+    way: function(way){
+        OSM_DATA_BLOB.way.push(way);
+    },
+    error: function(msg){
+        console.log('error: ' + msg);
+    }
+});
+
--- testOSM/main.js
+++ testOSM/main.js
@@ -1,98 +1,32 @@
 var osmread = require('osm-read');
-var http = require("http");
-var parseString = require('xml2js').parseString;
-/*
-function getXML(options, onResult)
-{
 
-
-    var prot = http;
-    var req = prot.request(options, function(res)
-    {
-        var output = '';
-        console.log(options.host + ':' + res.statusCode);
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-
-        res.on('end', function() {
-//            console.log(output);
-            onResult(output);
-           // onResult(res.statusCode, obj);
-        });
-    });
-
-    req.on('error', function(err) {
-        //res.send('error: ' + err.message);
-    });
-
-    req.end();
-};
-
-function nodeGeted(xml) {
-    console.log(xml);
-}
-
-function getNodes(xml) {
-
-    parseString(xml, function (err, result) {
-    
-        console.log(result);
-        
-        var nodeList = [];
-          
-        // We just want visible nodes
-        for (var i = 0; i < result.osm.node.length; ++i) {
-            var node =  result.osm.node[i]['$'];
-            if (node.visible) {
-                nodeList.push(node);
-                console.log(node);
-                
-                getXML({
-                    host: 'api.openstreetmap.org',
-                    port: 80,
-                    path: '/api/0.6/node/' + node.id,
-                    method: 'GET',
-                    headers: {
-                    'Authorization': 'Basic aGV4YXBvZGVzbXM6ZjlrYmZ1ang='
-                }}, nodeGeted)
-            }
-        }
-    });
-
-}
-
-getXML({
-    host: 'api.openstreetmap.org',
-    port: 80,
-    path: '/api/0.6/map?bbox=46.2183546,6.2664576,46.5383546,6.5864576',
-    method: 'GET',
-    headers: {
-        'Authorization': 'Basic aGV4YXBvZGVzbXM6ZjlrYmZ1ang='
-    }
-}, getNodes);
-
-*/
-
+var PROPERTY_VALUES = {};
 
 osmread.parse({
-    url: 'http://www.overpass-api.de/api/xapi?way[bbox=6.5694467,46.5201298,6.5794467,46.5301298]',
+    url: 'http://www.overpass-api.de/api/xapi?way[bbox=6.5594467,46.5101298,6.5794467,46.5301298]',
     format: 'xml',
     endDocument: function(){
-        console.log('document end');
+        console.log(PROPERTY_VALUES);
     },
     bounds: function(bounds){
-        console.log('bounds: ' + JSON.stringify(bounds));
+        //console.log('bounds: ' + JSON.stringify(bounds));
     },
     node: function(node){
-        console.log('node: ' + JSON.stringify(node));
+        // console.log('node: ' + JSON.stringify(node));
     },
     way: function(way){
-        console.log('way: ' + JSON.stringify(way));
+        // console.log('way: ' + JSON.stringify(way));
+      //  console.log(way.tags);
+        var key = '';
+        for (var itemK in way.tags){
+            key += itemK + '=' + way.tags[itemK] + ', ';
+        }
+        if (!PROPERTY_VALUES[key]) {
+            PROPERTY_VALUES[key] = way;
+        }
     },
     error: function(msg){
         console.log('error: ' + msg);
     }
-}); 
+});
+
