2dd3c990e3c4d922a22e24344d0b6f3d7b2adf28
dgellow <samuel.elborai@gmail.com>
2014-05-10 21:47:12 +0200

    I will get you down !

--- OSMRoad/main.rb
+++ OSMRoad/main.rb
@@ -14,6 +14,40 @@ def highways(data, tag = nil)
   end
 end
 
+def node_refs(data, ways)
+  ways.map do |w|
+    w['nodeRefs'].map do |nr|
+      node(data, nr)
+    end
+  end
+end
+
+def node(data, node_ref)
+  node = data['node'][node_ref.to_s]
+  lat, lon = node['lat'], node['lon']
+  OpenStruct.new({lat: lat, lon: lon})
+end
+
+def trace_way(list_nodes)
+  list_nodes.map do |nodes|
+    nodes.each_cons(2).map do |(node1, node2)|
+      concerned_cubes(node1, node2)
+    end
+  end
+end
+
+def slope(node1, node2)
+  node2.lat - node1.lat / node2.lon - node1.lon
+end
+
+def concerned_cubes(node1, node2)
+  Range.new(node1.lon, node2.lon).map do |n|
+    slope(node1, node2) * n
+  end
+end
+
+
+
 json_file = './OSMData.json'
 data = JSON.parse(File.open(json_file).read)
 highways = find_highway data
