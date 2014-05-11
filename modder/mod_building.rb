a0914e6e0559ada74f27ec327fc188bae26c91fc
dgellow <samuel.elborai@gmail.com>
2014-05-11 10:30:42 +0200

    auieauie

--- modder/mod_building.rb
+++ modder/mod_building.rb
@@ -1,3 +1,35 @@
-def process_building(_)
-  fail 'Not implemented'
+require './utils.rb'
+
+def process_building(list_nodes)
+  list_nodes.map do |nodes|
+    closing_segment = [nodes.first, nodes.last]
+    nodes.each_cons(2).to_a.push(closing_segment).map do |(node1, node2)|
+      x0, y0 = node1
+      x1, y1 = node2
+      height = node1.last
+      [line(x0, y0, x1, y1), height].flatten
+    end
+  end
+end
+
+def draw_building(elevations, list_cubes)
+  cubes_coordinates = list_cubes.map do |cubes|
+    cubes.map do |c|
+      x, y, height = c
+      x = 0
+      elev = elevations[x][y]
+      column = (elev..elev + height).map do |n|
+        [x, y, n]
+      end
+      column.flatten(1)
+    end
+  end
+
+  cubes_coordinates.flatten();
+end
+
+def get_node_building(data, way, node_ref)
+  height = way['tags']['height'] || Random.rand(3..20)
+  node = data['node'][node_ref.to_s]
+  [node['lat'].to_f, node['lon'].to_f, height.to_f]
 end
--- modder/mod_highway.rb
+++ modder/mod_highway.rb
@@ -1,24 +1,11 @@
-
-def slope(node1, node2)
-  x1, y1 = node1
-  x2, y2 = node2
-  (x2 - x1).abs / (y2 - y1).abs
-end
-
-def cubes_on_segment(node1, node2)
-  x1, _ = node1
-  x2, _ = node2
-
-  range = x1 < x2 ? (x1.to_i..x2.to_i) : (x2.to_i..x1.to_i)
-  range.map do |n|
-    [n, (slope(node1, node2) * n).to_i] rescue [nil, nil]
-  end
-end
+require './utils.rb'
 
 def process_highway(list_nodes)
   list_nodes.map do |nodes|
     nodes.each_cons(2).map do |(node1, node2)|
-      cubes_on_segment(node1, node2)
+      x0, y0 = node1
+      x1, y1 = node2
+      line(x0, y0, x1, y1)
     end
   end
 end
